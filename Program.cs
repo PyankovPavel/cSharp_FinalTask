58c76d728e9e3f71aaf58f0b6fc8becf8c648049
P1k4sh <p1k4sh@yandex.ru>
2022-09-05 15:22:11 +0300

    added Program.cs with final_Task

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string[] resultArray(string[] arr)
+{
+    int size = 0;
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        if (arr[i].Length < 4)
+        {
+            size++; // длина нового массива с элементами <=3 символов
+        }
+    }
+    string[] newArray = new string[size]; 
+    int index = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4)
+        {
+            newArray[index] = arr[i];
+            index++;
+        }
+    }
+    return newArray;
+}
+
+string[] array = { "Cat", "Dog", "Mother", "Father", "Sister", "Brother", "Son" };
+string[] array1 = { "223", "0_o", ":))", ":-В", "Sister", "Brother", "Son" };
+
+Console.WriteLine(String.Join(", ", resultArray(array)));
+Console.WriteLine(String.Join(", ", resultArray(array1)));
